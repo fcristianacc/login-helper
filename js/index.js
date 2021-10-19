00371817ee0d9b4c28ed718d55965dc823db1da3
Fátima Cristiana <fcristianacc@gmail.com>
2021-10-19 19:08:39 +0100

    Fixed JS file with JQuery

--- js/index.js
+++ js/index.js
@@ -1,20 +1,25 @@
-function showPassword() {
-    var eye = $('#eye');
-    var eyeSlash = $('#eye-slash');
-    var fieldPassword = $('#field-password');
+var eye = $('#eye');
+var eyeSlash = $('#eye-slash');
+var fieldPassword = $('#field-password');
 
-/*     eye.click(function() {
-        eye.hide();
-        eyeSlash.show();
-        fieldPassword.type = 'password';
-    })
+$('#btn-login').click(function(e) {
+    e.preventDefault();
+    alert('You are logged successfully!');
+});
 
-    eyeSlash.click(function() {
-        eye.show();
-        eyeSlash.hide();
-        fieldPassword.type = 'text';
-    }) */
+eye.click(function() {
+    eye.hide();
+    eyeSlash.show();
+    fieldPassword.get(0).type = 'password'; 
+});
 
+eyeSlash.click(function() {
+    eye.show();
+    eyeSlash.hide();
+    fieldPassword.get(0).type = 'text';
+});
+
+/* function togglePassword() {
     if (eye.style.display === 'none') {
         eye.style.display = 'block';
         eyeSlash.style.display = 'none';
@@ -24,9 +29,4 @@ function showPassword() {
         eyeSlash.style.display = 'block';
         fieldPassword.type = 'text';
     }
-}
-
-$('#btn-login').click(function(e) {
-    e.preventDefault();
-    alert('You are logged successfully!');
-});
+} */
